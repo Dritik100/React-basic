2860c497676607c6ce938f7126714771dd6903cd
Ritk Dubey <ritikdubey2664@gmail.com>
2024-10-07 16:41:14 +0530

    useCall hook

--- react-project-fifteen/src/components/ExpensiveComponents.jsx
+++ react-project-fifteen/src/components/ExpensiveComponents.jsx
@@ -1,18 +1,35 @@
 
-import React,{useState, useCallback} from 'react';
+import React,{useState, useCallback,useRef, useEffect} from 'react';
 
 const ExpensiveComponents = () => {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
+  const perviousFunction= useRef(null);
 
-  const expensiveCalculation = () => {
+  const expensiveCalculation = useCallback(() => {
     console.log("Running expensive calculation..");
     let result = 0;
     for (let i = 0; i < 1000000000; i++) {
       result += i;
     }
     return result;
-  };
+  }, [count]);
+
+    useEffect(() => {
+     if(perviousFunction.current){
+      if(perviousFunction.current === expensiveCalculation){
+        console.log("Function not re-created");
+      }
+      else{
+        console.log("Function got re-created");
+      }
+
+     }
+     else{
+      perviousFunction.current = expensiveCalculation;
+     }
+    }, [expensiveCalculation]);
+    
 
   return (
     <div>
